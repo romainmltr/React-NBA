ff7209cc242140aab9dbf4f91bbde05652be72e1
[GrandEmpereur] <[bartosikpatrick2@gmail.com]>
2023-02-01 09:37:41 +0100

    :memo: Update setup

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from '../assets/react.svg'
+import '../scss/views/App.scss'
 
 function App() {
   const [count, setCount] = useState(0)
