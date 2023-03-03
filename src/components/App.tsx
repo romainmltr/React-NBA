e593b7ee08fe1bba33ed9670ac8c03c8e8d4a560
[GrandEmpereur] <[bartosikpatrick2@gmail.com]>
2023-03-03 16:27:56 +0100

    :bug: fix build bug

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,25 +3,24 @@ import { AuthContext } from "../context/AuthContext";
 import { useContext } from "react";
 
 function App() {
+	const user = useContext(AuthContext);
 
-    const user = useContext(AuthContext);
-    
-    if (user.isLogged) {
-        return (
-            <div className="App">
-                <Filter />
-            </div>
-        )
-    }
-    if (!user) return (
-        <div className="App text-center">
-            <h1 className="mb-10">Not logged</h1>
+	if (user && user.isLogged) {
+		return (
+			<div className="App">
+				<Filter />
+			</div>
+		);
+	}
 
-            <a href="/" className="text-white">
-                <button>Go to login page</button>
-            </a>
-        </div>
-    )
+	return (
+		<div className="App text-center">
+			<h1 className="mb-10">Not logged</h1>
+			<a href="/" className="text-white">
+				<button>Go to login page</button>
+			</a>
+		</div>
+	);
 }
 
-export default App
+export default App;
